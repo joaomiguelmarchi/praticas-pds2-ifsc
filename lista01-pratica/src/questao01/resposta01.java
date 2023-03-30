dd46cced4083eb023c24597877a0f3b967729776
Joao <jmsmarchi@gmail.com>
2023-03-30 08:17:39 -0300

    ...

--- lista01-pratica/src/questao01/resposta01.java
+++ lista01-pratica/src/questao01/resposta01.java
@@ -14,16 +14,17 @@ public class resposta01 {
 			Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/"+ "?serverTimezone=UTC","root","aluno");
 			String queryCreateDatabase = "CREATE DATABASE"+ databaseName;
 			String queryCreateTable = "CREATE TABLE IF NOT EXISTS pessoa (\r\n"
-					+ " nome VARCHAR(50) AUTO_INCREMENT PRIMARY KEY, \r\n"
+					+ " nome VARCHAR(50)PRIMARY KEY, \r\n"
 					+ " idade INTEGER NOT NULL, \r\n"
 					+ " email VARCHAR(100) NOT NULL, \r\n"
 					+ ")";
 			Statement stm = conexao.createStatement();
 			stm.executeQuery(queryCreateDatabase);
 			
-			conexao.close();
 		}catch (SQLException e){
 			e.printStackTrace();
+		}finally {
+			conexao.close();
 		}
 		
 		
